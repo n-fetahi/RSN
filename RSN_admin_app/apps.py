91b66aea4fcd9f12034978b3ac169cd6c037b061
NOORALDEIN FETAHI <117300676+n-fetahi@users.noreply.github.com>
2023-06-25 05:27:58 +0300

    Delete RSN_admin_app directory

--- RSN_admin_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RsnAdminAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'RSN_admin_app'
--- RSN_admin_app/urls.py
+++
@@ -1,4 +0,0 @@
-from django.urls import path
-from RSN_admin_app import views
-
-urlpatterns=[]
